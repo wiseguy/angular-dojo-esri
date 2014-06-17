e8902bc7bb2a924b22a8de69e979a91e528beae6
Aamir Suleman <asuleman@blueraster.com>
2014-06-17 09:20:13 -0400

    adding bower support

--- src/app/js/map/controller/mapcontroller.js
+++ src/app/js/map/controller/mapcontroller.js
@@ -3,22 +3,27 @@ define([], function() {
     (function() {
         angular.module("app.map", [])
             .controller('MapCtrl', function($scope, $rootScope, mapService) { //separate this to a service
-
+                var theData;
                 mapService.then(function(data) {
 
-                    $scope.data = data;
+                    //$scope.data = data;
+                    theData = data;
+                    console.log(data);
 
                 })
                 $scope.mapCreated = false;
                 $scope.createMap = function() {
 
                     //only if view is map, and only once
-
-                    if (!$scope.mapCreated) {
+                   //alert($scope.map);
+                    if ((!$scope.map && $scope.mapCreated) || !theData || $scope.mapCreated) {
+                        //alert("cant be created");
                         return;
                     }
                     // alert("isolate scope create map");
-                    alert($scope.map);
+                    console.log($scope.map);
+                    console.log($scope.mapCreated);
+                    console.log("Creating map");
                     $scope.mapCreated = true;
 
                     // alert("create map view");
@@ -26,15 +31,19 @@ define([], function() {
                         "esri/SpatialReference", "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol", "dojo/_base/Color"
                     ], function(Map, registry, ContentPane, Graphic, Polygon, SpatialReference, SimpleFillSymbol, SimpleLineSymbol, Color) {
 
-                        var data = $scope.data;
+                        var data = theData;//$scope.data;
 
                         console.log(" got data in map directive ");
+                        console.log(" Creating map only one time ");
+
+                        console.log(data);
+                        
                         var sfs = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
                             new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
                                 new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.25]));
                         new ContentPane({
                             id: "mapCP",
-                            style: "width:100%;height:100%"
+                            style: "width:300px;height:200px"
                         }, "mapDiv");
                         var map = new Map("mapCP", {
                             "id": "myMap",
